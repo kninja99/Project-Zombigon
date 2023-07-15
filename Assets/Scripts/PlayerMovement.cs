c5e53b8d7832b0c2a4a314d651092f3009a79bee
Kninja99 <kcdamschen@yahoo.com>
2023-07-14 21:29:50 -0700

    PlayerController with camer movement
    * added a camera that will move with mouse
    
    * edited movement script to work with a player controller

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,36 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     // default moving speed for players
     [SerializeField] private float moveSpeed = 12f;
-
+    [SerializeField] private CharacterController controller;
     // Update is called once per frame
     private void Update()
     {
-        Vector2 inputVector = new Vector2(0, 0);
-        // Groud movement for player
-        if(Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y = -1;
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x = -1;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x = +1;
-        }
-        // normalize input vector
-        inputVector = inputVector.normalized;
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
         // actual movement
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir * moveSpeed * Time.deltaTime;
+        Vector3 moveDir = transform.right * x + transform.forward * z;
+        controller.Move(moveDir * moveSpeed * Time.deltaTime);
     }
 }
